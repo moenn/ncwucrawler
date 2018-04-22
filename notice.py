3bfcb8cae5bb4fdba9ae4cf43b2e24b68fcb9a2d
mozjiang <jms292569413@sina.com>
2018-04-22 23:00:47 +0800

    add page_num key to dictionary used to store data about the website

--- notice.py
+++ notice.py
@@ -7,7 +7,7 @@
 依赖:
 requests
 bs4
-
+lxml
 2018/4/15
 '''
 
@@ -39,6 +39,7 @@ HEADERS = {
 
 HOMEURL = "http://www5.ncwu.edu.cn/channels/5.html"
 
+# INTERVAL = 5
 
 def get_endpage_maxnum(homeurl):
     resp = requests.get(homeurl, headers=HEADERS)
@@ -88,39 +89,56 @@ def create_pageurl_list(startpage_num, endpage_num):
 
     return pageurl_list
 
-
+# @profile
 def get_data_from_pageurl(pageurl):
     '''
     visit a page url, and return a dict which includes 12 notices's data.  
     '''
     print("resolve {}...\n".format(pageurl))
+
+    if pageurl == 'http://www5.ncwu.edu.cn/channels/5.html':
+        page_num = 1
+    else:
+        page_num = re.search(r'http://www5.ncwu.edu.cn/channels/5_(?P<page_num>\d+).html', pageurl).group('page_num')
+
     resp = requests.get(pageurl, headers=HEADERS)
     resp.encoding = 'utf-8'
     pattern = re.compile(
         # department
-        r'<li>\s+【<a href="http://www5.ncwu.edu.cn/channels/\d*.html" class="dw">(.+)'
+        r'<li>\s+【<a href="http://www5.ncwu.edu.cn/channels/\d*.html" class="dw">(?P<department>.+)'
         # href
-        r'</a>】\s+<a href="http://www5.ncwu.edu.cn/contents/(.+)\.html"'
+        r'</a>】\s+<a href="http://www5.ncwu.edu.cn/contents/(?P<href>.+)\.html"'
         # title and create_time
-        r' target="_blank"><span> (.+) </span> </a><i>(\d{4}-\d{2}-\d{2})</i></li>'
+        r' target="_blank"><span> (?P<title>.+) </span> </a><i>(?P<create_time>\d{4}-\d{2}-\d{2})</i></li>'
     )
-    result = re.finditer(pattern, resp.text)
+    matches = re.finditer(pattern, resp.text)
 
-    data = {'http://www5.ncwu.edu.cn/contents/{}.html'.format(n.group(2)): {'department': n.group(1), 'title': n.group(
-        3), 'href': 'http://www5.ncwu.edu.cn/contents/{}.html'.format(n.group(2)), 'create_time': n.group(4)} for n in result}
+    data = {}
+    for match in matches:
+        href = "http://www5.ncwu.edu.cn/contents/{}.html".format(match.group('href'))
+        data[href] = {}
+        
+        data[href]['page_num'] = page_num
+        data[href]['department'] = match.group('department')
+        data[href]['title'] = match.group('title')
+        data[href]['href'] = href
+        data[href]['create_time'] = match.group('create_time')
 
     return data
-
-
+# @profile
 def get_data_from_pageurl_list(pageurl_list):
     result = {}
 
-    for n in map(get_data_from_pageurl, pageurl_list):
-        result.update(n)
 
+    for pageurl in pageurl_list:
+        data = get_data_from_pageurl(pageurl)
+        result.update(data)
+        time.sleep(1)
     return result
 
-def get_text_and_upload_href(noticeurl):
+
+# @profile
+def get_text_and_upload(noticeurl):
     resp = requests.get(noticeurl, headers=HEADERS)
     try:
         soup = BeautifulSoup(resp.content, 'lxml')
@@ -128,53 +146,46 @@ def get_text_and_upload_href(noticeurl):
         soup = BeautifulSoup(resp.content, 'html.parser')
 
     # get text
-    # children includes tag and navigablestring
     xinxi_con_children = soup.find('div', class_='xinxi_con').children
 
     paragraphs = [n.text for n in xinxi_con_children if n.name == 'p']
     text = '\n'.join(paragraphs)
 
-    # get upload href
+    # get upload
 
-    # upload_pattern = re.compile(r'http://www5.ncwu.edu.cn/upload/files/.+\..+')
     upload_pattern = re.compile(r'http://www5.ncwu.edu.cn/upload/.+')
     # string_pattern = re.compile(r'^(?!附件\d+).*$')
-    # upload_result = soup.find_all('a', href=upload_pattern, string=string_pattern)
 
     upload_result = soup.find_all('a', href=upload_pattern)
 
     if upload_result:
-        upload_href = {n['href']: n.text for n in upload_result}
+        upload = {n['href']: n.text for n in upload_result}
 
     else:
-        upload_href = {}
+        upload = {}
 
-    return text, upload_href
+    return text, upload
 
-
-def add_text_and_upload_href(data):
+# @profile
+def add_text_and_upload(data):
     for key in data.keys():
-        text, upload_href = get_text_and_upload_href(key)
+        text, upload = get_text_and_upload(key)
         data[key]['text'] = text
-        data[key]['upload_href'] = upload_href
+        data[key]['upload'] = upload
 
     return data
 
 
 
 
-
-def download(data, startpage_num, root_dir):
-    count = 0
+def download(data, root_dir):
 
     for key, value in data.items():
 
-        if count % 12 == 0:
-            page_dir_path = os.path.join(
-                root_dir, 'notice_5_' + str(startpage_num))
+        
+        page_dir_path = os.path.join(root_dir, 'notice_5_' + str(value['page_num']))
+        if not os.path.exists(page_dir_path):
             os.mkdir(page_dir_path)
-            startpage_num += 1
-        count += 1
 
         file_name = '[{}][{}][{}]'.format(
             value['department'], value['title'], value['create_time'])
@@ -182,46 +193,47 @@ def download(data, startpage_num, root_dir):
         file_name = file_name.replace('\\', '')
 
         # upload file existed, create a dir to save text and upload file
-        if value['upload_href']:
+        if value['upload']:
             save_dir_path = os.path.join(page_dir_path, file_name)
             os.mkdir(save_dir_path)
             text_save_path = os.path.join(page_dir_path, file_name, file_name)
 
-            # print("save {}...\n".format(text_save_path))
+            print("save {}...\n".format(text_save_path))
             with open(text_save_path + '.txt', 'w', encoding='utf-8') as f:
                 f.write(value['text'])
 
             # save upload file
-            for url, name in value['upload_href'].items():
+            for href, name in value['upload'].items():
                 try:
                     save_path = os.path.join(save_dir_path, name)
-                    # print("save {}... \n".format(save_path))
+                    print("save {}... \n".format(save_path))
                     with open(save_path, 'wb') as f:
-                        f.write(requests.get(url).content)
+                        f.write(requests.get(href).content)
                 except:
-                    logging.critical('下载 {}[{}] 附件失败\n'.format(name, url))
+                    logging.critical('下载 {}[{}] 附件失败\n'.format(name, href))
 
         # no upload file , only save text
         else:
             text_save_path = os.path.join(page_dir_path, file_name)
-            # print("save {}...\n".format(text_save_path))
+            print("save {}...\n".format(text_save_path))
             with open(text_save_path + '.txt', 'w', encoding='utf-8') as f:
                 f.write(value['text'])
 
-        time.sleep(5)
-
+        time.sleep(2)
 
 def main():
-
-    root_dir = datetime.now().strftime('%Y%m%d_%H_%M_%S_ncwu_notice')
+    time_string= datetime.now().strftime('%Y%m%d_%H_%M_%S')
+    
+    root_dir = '{}_ncwu_notice'.format(time_string)
     os.mkdir(root_dir)
+    root_dir_fullpath = os.path.join(os.getcwd(), root_dir)
+
     log_path = os.path.join(
         root_dir, datetime.now().strftime('%Y%m%d_%H_%M_%S') + '.log')
     logging.basicConfig(filename=log_path, level=logging.WARNING,
                         format='%(asctime)s %(message)s')
 
-    INTERVAL = 5
-
+    log_full_path = os.path.join(os.getcwd(), log_path)
     print("正在获取 ncwu 的学校通知栏的页面范围...\n")
 
     ENDPAGE_MAXNUM = get_endpage_maxnum(HOMEURL)
@@ -236,23 +248,26 @@ def main():
     print("初始化完成,设置为：\n")
     print("页面总范围：1 - {} \n".format(ENDPAGE_MAXNUM))
     print("下载范围：{} - {} \n".format(startpage_num, endpage_num))
-    print("睡眠间隔：{}s \n".format(INTERVAL))
-    print("下载文件夹路径 : {}\n".format(os.path.join(os.getcwd(), root_dir)))
-    print("log file 路径 :{} \n".format(os.path.join(os.getcwd(), log_path)))
+    # print("睡眠间隔：{}s \n".format(INTERVAL))
+    print("下载文件夹路径 : {}\n".format(root_dir_fullpath))
+    print("log file 路径 :{} \n".format(log_full_path))
     print("程序将在 5 s 后开始下载...\n")
     print("按 Ctrl + C 取消下载.\n`")
 
     time.sleep(5)
 
     data = get_data_from_pageurl_list(pageurl_list)
-    data = add_text_and_upload_href(data)
-    download(data, startpage_num, root_dir)
+    data = add_text_and_upload(data)
+    download(data, root_dir)
 
     print('----------------------------------\n')
-    print("下载完成，下载文件夹路径为: {}\n".format(os.path.join(os.getcwd(), root_dir)))
-    print("下载失败信息存储在 log file ， 路径为: {} \n".format(
-        os.path.join(os.getcwd(), log_path)))
+    print("下载完成，下载文件夹路径为: {}\n".format(root_dir_fullpath))
+    print("下载失败信息存储在 log file ， 路径为: {} \n".format(log_full_path))
 
 
 if __name__ == '__main__':
     main()
+    # pageurl_list = create_pageurl_list(120, 121)
+    # data = get_data_from_pageurl_list(pageurl_list)
+    # for k, v in data.items():
+    #     print(k, v['page_num'],'\n')
